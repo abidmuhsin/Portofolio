3888fff01d7b56faaca56bae92cee4d9ce07fd8f
Abid <abidmhsn@gmail.com>
2024-08-12 12:02:33 +0800

    update

--- src/container/ProjectCard.jsx
+++ src/container/ProjectCard.jsx
@@ -1,9 +1,9 @@
 const ProjectCard = ({ ImageUrl, TitleProject, ProjectDescription, ButtonOnclick }) => {
     return (
         <div className="max-w-80 max-h-80  m-4 p-4 flex flex-col justify-center items-start shadow-2xl rounded bg-slate-800 text-white gap-1">
-            <img src={ImageUrl} alt="Project" className="h-[30vh] bg-white object-cover"/>
-            <h2 className="text-xl font-bold">{TitleProject}</h2>
-            <p className="text-sm text-yellow-500 text-justify">{ProjectDescription}</p>
+            <img src={ImageUrl} alt="Project" className="h-[40vh] bg-white object-cover"/>
+            <h2 className="text-xl font-bold w-full h-10">{TitleProject}</h2>
+            <p className="text-xs text-yellow-500 text-justify w-full h-[8vh] ">{ProjectDescription}</p>
             <button onClick={ButtonOnclick} className="bg-yellow-800 text-white font-bold px-5 py-2 my-3 hover:bg-yellow-900">View</button>
         </div>
     )
