b985963a83b20f12aa1306b8b2489de02543b10a
Abid <abidmhsn@gmail.com>
2024-08-12 12:07:29 +0800

    update

--- src/elements/Navbar.jsx
+++ src/elements/Navbar.jsx
@@ -3,9 +3,8 @@ const Navbar = () => {
         <div className="h-[10vh] bg-slate-900 flex items-center justify-end">
             <div className="flex items-center mr-4 justify-end">
                 <h1 className="text-[12px] text-white font-semibold hover:text-teal-200 mx-5"><a href="">HOME</a></h1>
-                <h1 className="text-[12px] text-white font-semibold hover:text-teal-200 mx-5"><a href="">ABOUT</a></h1>
-                <h1 className="text-[12px] text-white font-semibold hover:text-teal-200 mx-5"><a href="">PROJECT</a></h1>
                 <h1 className="text-[12px] text-white font-semibold hover:text-teal-200 mx-5"><a href="">EXPERIENCE</a></h1>
+                <h1 className="text-[12px] text-white font-semibold hover:text-teal-200 mx-5"><a href="">PROJECT</a></h1>
                 <h1 className="text-[12px] text-white font-semibold hover:text-teal-200 mx-5"><a href="">CONTACT</a></h1>
             </div>
         </div>    
